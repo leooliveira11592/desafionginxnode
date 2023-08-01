62b3c493dfb84e0be7570d6c7f797f6321241df7
leooliveira11592 <leooliveira1159@gmail.com>
2023-08-01 09:24:36 -0300

    Adicionando arquivos do desafio nodenginx

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const config = {
-    host: 'banco',
+    host: 'db',
     user: 'root',
     password: 'root',
     database: 'nodedb'
@@ -13,28 +13,10 @@ const mysql = require('mysql');
 
 app.get('/', (req, res) => {
 
-    console.log("entoru - 1");
-    console.log("entoru - 2");
-
-    // insere a pessoa
     const connection = mysql.createConnection(config);
-    const sql = `INSERT INTO PEOPLE(NAME) VALUES('LEONARDO J OLIVEIRA');`
-    connection.query(sql);
-    connection.query(sql, function (err, rows, fields) {
-        if (err) {
-          console.log(`Insert -> ${err}\nCode:${err.code}\nSQL:${err.sql}\n\n\n`);
-        }
-      });
-    // connection.end()
-
-    console.log("entoru - 3");
 
     // retorna a pessoa inserida
-    // const connection2 = mysql.createConnection(config)
-    console.log("entoru - 4");
-    const query = `SELECT * FROM PEOPLE`;
-
-    console.log("entoru - 5");
+    const query = `SELECT * FROM PEOPLE ORDER BY NAME`;
 
     connection.query(query, function (err, result, fields) {
         if (err) {
@@ -42,14 +24,20 @@ app.get('/', (req, res) => {
             console.log(`Select -> ${err}\nCode:${err.code}\nSQL:${err.sql}`);
         }
         console.log(result);
-        res.send('<h1>Full Cycle Rocks!</h1><br><br>' + result);
+        var html = '<h1>Full Cycle Rocks!</h1><br><br>';
+        
+        for (let pessoa of result) {
+          html += `<h3>${pessoa.ID} - ${pessoa.NAME}</h3><br>`;
+        }
+
+        res.send(html);
       });
 
-    console.log("entoru - 6");
     connection.end()
 })
 
 app.listen(port, () => {
-    console.log('rodando na porta ' + port)
-    console.log('Chame no navegador: http://localhost:8080')
+  console.log('==========================================')
+  console.log('Rodando internamente na porta ' + port)
+  console.log('Chame no navegador: http://localhost:8080')
 })
